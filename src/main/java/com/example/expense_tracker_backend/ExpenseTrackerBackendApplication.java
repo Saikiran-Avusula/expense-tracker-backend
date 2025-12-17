7b86a6623f1587403c14f3c3941955fea610f52f
Saikiran-Avusula <saikiranavusula89@gmail.com>
2025-12-17 17:15:15 +0530

    updated file

--- src/main/java/com/example/expense_tracker_backend/ExpenseTrackerBackendApplication.java
+++ src/main/java/com/example/expense_tracker_backend/ExpenseTrackerBackendApplication.java
@@ -8,11 +8,10 @@ public class ExpenseTrackerBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerBackendApplication.class, args);
-<<<<<<< HEAD
+
 		System.out.println();
 		System.out.println("Running successfully");
-=======
->>>>>>> 621495d73e214249a5e6fd4309e31b8c8d84135e
+
 	}
 
 }
